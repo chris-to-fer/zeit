98fa6332d26cfce4a5a74eab6c55f89aac96b7de
chris-to-fer <mail@christopherhaug.de>
2024-02-20 17:22:37 +0100

    added mui and first table

--- src/app/[userId]/projects/[proId]/employees/[empId]/week/[weekId]/page.js
+++ src/app/[userId]/projects/[proId]/employees/[empId]/week/[weekId]/page.js
@@ -2,6 +2,7 @@ import React from "react";
 import { revalidatePath } from "next/cache";
 import Sidebar from "@/app/[userId]/projects/components/Sidebar";
 import styles from "../../../../../page.module.css";
+import DataTable from "./components/weekTable";
 
 export default function PageWeek({ params, children }) {
   const HOSTNAME = process.env.HOSTNAME_URL;
@@ -11,6 +12,7 @@ export default function PageWeek({ params, children }) {
       <Sidebar proId={proId} />
       <div className={styles.card_project}>
         <h3>Woche</h3>
+        <DataTable />
       </div>
       {children}
     </>
